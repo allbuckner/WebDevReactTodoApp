04cde1e923149451ea8f16021a4ae2f6be9eb031
Allen Buckner <abuckner@wustl.edu>
2018-11-25 22:41:42 -0600

    Made some changes to fix performance

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
 import Todo from './Todo';
-import NewTodo from './NewTodo'
+import NewTodo from './NewTodo';
 
 class App extends Component {
   render() {
     return (
-        <section id="todos">
-        <NewTodo />
-        <Todo />
-          <aside id="newtodo">
-            <div id="instr">
-              <p>Instructions: 1.Write a ToDo 2.Hit Submit 3.Refresh to see your Todo on the Page!</p>
-            </div>
-            <form id="todo-form">
-                <input id="newTitle"  type="text"  placeholder="Type a New ToDo..."/>
-                <button id="newsubmit">+</button>
-            </form>
-          </aside>
-        </section>
+      <div className="App">
+        <div id="topbar">
+          <div id="heading">
+            <p>Allen Buckners To-Do Application</p>
+          </div>
+        </div>
+        <div id="main">
+          <div id="submain">
+            <section id="todos">
+              <NewTodo />
+              <Todo />
+            </section>
+          </div>
+        </div>
+      </div>
     );
   }
 }
